29bfe38f57080fca79c07844d5f37bd60272cf3d
Gurkeet Puri <748159@student.tor.loc>
2019-08-07 15:45:22 -0400

    third commit

--- index.js
+++ index.js
@@ -2,5 +2,10 @@ const http = require('http');
 
 const site = http.createServer(function(req, res) {
     console.log("Hello World");
+    console.log(req.headers);
+    console.log(req.url);
+    res.setHeader('Content-Type', application / json);
+    res.write(JSON.stringify(data));
+    res.end();
 });
 site.listen(3000);
